d0f64645b5d9a6bb0287fccf8aa434a63353972d
Kateryna <moskkat40@gmail.com>
2024-11-13 17:46:42 +0100

    add react autocomplete task

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,65 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { DropdownMenu } from './components/DropdownMenu';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [title, setTitle] = useState(selectedPerson?.name || '');
+
+  console.log(selectedPerson)
+
+  useEffect(() => {
+    setTitle(selectedPerson?.name || '');
+  }, [selectedPerson]);
+
+  const filteredUsers = useMemo(() => {
+    return [...peopleFromServer].filter(person => person.name.includes(title));
+  }, [title]);
+
+  const [inputFieldFocus, setInputFieldFocus] = useState(false);
+
+  function handleTitle(event: React.ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
+  }
+
+  function handleInputFieldFocus() {
+    setInputFieldFocus(true);
+  }
+
+  function handleInputFieldBlur() {
+    setInputFieldFocus(false);
+    setTitle('');
+  }
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {selectedPerson === null || title !== selectedPerson.name
+            ? 'No selected person'
+            : `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`}
         </h1>
 
         <div className="dropdown is-active">
           <div className="dropdown-trigger">
             <input
+              value={title}
               type="text"
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              onChange={handleTitle}
+              onClick={handleInputFieldFocus}
             />
           </div>
-
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
+          {inputFieldFocus && (
+            <DropdownMenu
+              peopleFromServer={filteredUsers}
+              onSelect={setSelectedPerson}
+            />
+          )}
         </div>
       </main>
     </div>
